9171ddb863759df55393b5e609defa53620271c0
Otávio Pace <otaviopp8@gmail.com>
2018-04-25 16:04:23 -0300

    update builded library

--- lib/queue.d.ts
+++ lib/queue.d.ts
@@ -10,12 +10,15 @@ export interface ProcessOptions {
     oneShot?: boolean;
     keepMessages?: boolean;
 }
+export interface Params {
+    MessageGroupId?: String;
+}
 export declare class Queue<TItem> extends EventEmitter {
     private options;
     private running;
     private stopped;
     constructor(options: QueueOptions);
-    push(item: TItem): Promise<void>;
+    push(item: TItem, parameters?: Params): Promise<void>;
     remove(message: SQS.Message): Promise<void>;
     startProcessing(handler: (item: TItem, message: SQS.Message) => any | PromiseLike<any>, options?: ProcessOptions): PromiseLike<void>;
     stopProcessing(): PromiseLike<void>;
